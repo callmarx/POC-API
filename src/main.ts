46a79151327a4d1e28c9c062e3a6dedb877ee528
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2021-10-10 19:03:45 -0300

    setup HTTP request logger middleware - morgan

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from "morgan";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan("tiny"));
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
